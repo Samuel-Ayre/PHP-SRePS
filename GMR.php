cd3fa9cb7c0f359a5e8e936578d48c9c58590768
jaxon63 <jaxon63@gmail.com>
2016-09-04 18:19:25 +1000

    fixed

--- GMR.php
+++ GMR.php
@@ -3,52 +3,52 @@
 
 	switch ($month) {
 		case 'January':
-			$month = "MONTH(date) = 1";
+			$month = "MONTH(Date) = 1";
 			break;
 		case 'February':
-			$month = "MONTH(date) = 2";
+			$month = "MONTH(Date) = 2";
 			break;
 		case 'March':
-			$month = "MONTH(date) = 3";
+			$month = "MONTH(Date) = 3";
 			break;
 		case 'April':
-			$month = "MONTH(date) = 4";
+			$month = "MONTH(Date) = 4";
 			break;
 		case 'May':
-			$month = "MONTH(date) = 5";
+			$month = "MONTH(Date) = 5";
 			break;
 		case 'June':
-			$month = "MONTH(date) = 6";
+			$month = "MONTH(Date) = 6";
 			break;
 		case 'July':
-			$month = "MONTH(date) = 7";
+			$month = "MONTH(Date) = 7";
 			break;
 		case 'August':
-			$month = "MONTH(date) = 8";
+			$month = "MONTH(Date) = 8";
 			break;
 		case 'September':
-			$month = "MONTH(date) = 9";
+			$month = "MONTH(Date) = 9";
 			break;
 		case 'October':
-			$month = "MONTH(date) = 10";
+			$month = "MONTH(Date) = 10";
 			break;
 		case 'November':
-			$month = "MONTH(date) = 11";
+			$month = "MONTH(Date) = 11";
 			break;
 		case 'December':
-			$month = "MONTH(date) = 12";
+			$month = "MONTH(Date) = 12";
 			break;
 		default:
 			break;
 	}
-	$month += " AND YEAR(date) = 2016";
+	$month .= " AND YEAR(Date) = 2016";
 	
 	$conn = mysqli_connect('110.142.49.152:3306/', 'php3', 'php', 'PHP_SREPS');
 		if (!$conn)
 			echo "<p>Couldn't connect to database</p>";
 		else
 			{
-			$query = "Select * from SalesData where $month";
+			$query = "Select * from salesdata where $month";
 			
 			$result = mysqli_query($conn, $query);
 			
@@ -57,8 +57,11 @@
 			else
 			{
 				$reference = mysqli_fetch_row($result);
-
-				echo "$reference";
+				while($reference)
+				{
+					echo "$reference[0], $reference[1], $reference[2], $reference[3] \n";
+					$reference = mysqli_fetch_row($result);
+				}
 			}
 		}
 		mysqli_close($conn);
