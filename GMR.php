5624c06e38d0069c9011d1f59edf9ad2903a3feb
Samuel-Ayre <s-ayre@hotmail.com>
2016-09-25 19:43:12 +1000

    Updates as of 25th
    now generates reports, still a bit to improve but is generating reports,
    and neds to test

--- GMR.php
+++ GMR.php
@@ -35,82 +35,36 @@
 					</tr>
 				</table>
 			</form>
-			<h3>All Item Report</h3>
-			<form id="allsearch"  method="GET">
+			<form id="Cutomer Range" method="GET">
 				<table>
 					<tr>
-						<td><select name="Month" id="Month">
-							<option Value="January">January</option>
-							<option Value="February">February</option>
-							<option Value="March">March</option>
-							<option Value="April">April</option>
-							<option Value="May">May</option>
-							<option Value="June">June</option>
-							<option Value="July">July</option>
-							<option Value="August">August</option>
-							<option Value="September">September</option>
-							<option Value="October">October</option>
-							<option Value="November">November</option>
-							<option Value="December">December</option>
-						</select>
-						<td><input type=submit name="allsubmit" value="Submit"></input></td>
+						<td>Custom Dates</td>
+					</tr>
+					<tr>
+						<td><input type="Date" name="FDate"></td>
+					</tr>
+					<tr>
+						<td><input type="Date" name="LDate"></td>
+					</tr>
+					<tr>
+						<td><input type="submit" name="CSubmit" value="Submit"></td>
 					</tr>
 				</table>
+
 			</form>
 
+<?php 
+		
+	if (isset($_GET['FDate'], $_GET['LDate'], $_GET['CSubmit'])) {
+		$FDate = date("Y-m-d", strtotime($_GET['FDate']));
+		$LDate = date("Y-m-d", strtotime($_GET['LDate']));
 
-<?php
-	if (isset($_GET['Month'])) {
-	$month = $_GET['Month'];
-
-	switch ($month) {
-		case 'January':
-			$month = "MONTH(Date) = 1";
-			break;
-		case 'February':
-			$month = "MONTH(Date) = 2";
-			break;
-		case 'March':
-			$month = "MONTH(Date) = 3";
-			break;
-		case 'April':
-			$month = "MONTH(Date) = 4";
-			break;
-		case 'May':
-			$month = "MONTH(Date) = 5";
-			break;
-		case 'June':
-			$month = "MONTH(Date) = 6";
-			break;
-		case 'July':
-			$month = "MONTH(Date) = 7";
-			break;
-		case 'August':
-			$month = "MONTH(Date) = 8";
-			break;
-		case 'September':
-			$month = "MONTH(Date) = 9";
-			break;
-		case 'October':
-			$month = "MONTH(Date) = 10";
-			break;
-		case 'November':
-			$month = "MONTH(Date) = 11";
-			break;
-		case 'December':
-			$month = "MONTH(Date) = 12";
-			break;
-		default:
-			break;
-	}
-	$month .= " AND YEAR(Date) = 2016";
-	
 	$conn = mysqli_connect('192.168.183.128:3306/', 'php3', 'php', 'PHP_SREPS');
 		if (!$conn)
 			echo "<p>Couldn't connect to database</p>";
 		else
 			{
-			$query = "SELECT SaleNo, inventorydata.INVName, Date, AmountSold FROM salesdata INNER JOIN inventorydata ON salesdata.InvNo = inventorydata.INVNO where $month";
+			$query = "SELECT SaleNo, inventorydata.INVName, Date, AmountSold FROM salesdata INNER JOIN inventorydata ON salesdata.InvNo = inventorydata.INVNO where Date BETWEEN '$FDate' AND '$LDate'";
 			
 			$result = mysqli_query($conn, $query);
 			
@@ -141,9 +95,10 @@
 			}
 		}
 		mysqli_close($conn);
+		echo "<input type='submit' name='Export' value='Export to CSV File'>";
 	}
 ?>
-
+				
 		</Div>
 	</div>
 	</body>
